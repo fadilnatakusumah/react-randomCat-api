dc090e2815d9f0197e195f4c03bb928cdfcb874f
fadilnatakusumah <fadil.ntksmh@gmail.com>
2018-10-30 13:47:20 +0700

    adding production folder

---
+++ docs/precache-manifest.a7c858851f8b41a700dd7b91501cd4aa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd00e5030ffef9898f1cddb7e3447975",
+    "url": "/static/media/cubicblock.bd00e503.woff"
+  },
+  {
+    "revision": "ada03e4ea351b388a007",
+    "url": "/static/css/main.6b9e844e.chunk.css"
+  },
+  {
+    "revision": "515fca8d9da9300b13b60ca0bb26113c",
+    "url": "/static/media/quick_end_jerk.515fca8d.woff"
+  },
+  {
+    "revision": "4b2ed1d19835639500bd",
+    "url": "/static/js/1.4b2ed1d1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "80c87699c146668aea7668e5941da260",
+    "url": "/static/media/cubicblock_d.80c87699.woff"
+  },
+  {
+    "revision": "ada03e4ea351b388a007",
+    "url": "/static/js/main.ada03e4e.chunk.js"
+  },
+  {
+    "revision": "b2cc5415049c106418e4e26275e5b794",
+    "url": "/static/media/cubicblock_b.b2cc5415.woff"
+  },
+  {
+    "revision": "8a4bb1933c9c1eeeb813be7e531b709b",
+    "url": "/static/media/cubicblock_s.8a4bb193.woff"
+  },
+  {
+    "revision": "fc8e4b44b58218d3684cf6812233b9fb",
+    "url": "/static/media/cubicblock_t.fc8e4b44.woff"
+  },
+  {
+    "revision": "4b2ed1d19835639500bd",
+    "url": "/static/css/1.a51a1468.chunk.css"
+  },
+  {
+    "revision": "b2d901157f84389493a6a6f121731fd7",
+    "url": "/index.html"
+  }
+];
